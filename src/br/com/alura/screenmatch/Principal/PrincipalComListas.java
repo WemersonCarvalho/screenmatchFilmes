9d314a0eb33ec58cd01ec7b446b47c3e7ecc8d54
Wemerson <wemerson.carvalho@ufrpe.br>
2025-01-07 20:59:18 -0300

    Initial commit

---
+++ src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.Principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+
+    }
+}
